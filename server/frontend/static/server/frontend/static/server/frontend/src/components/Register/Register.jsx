dae642fb86f9295352de57e5d7bfc8e5d7f25f2d
youssefahmed505 <cananyonepvpme2@gmail.com>
2026-06-16 23:07:06 +0300

    Register.jsx

--- server/frontend/static/server/frontend/static/server/frontend/src/components/Register/Register.jsx
+++ server/frontend/static/server/frontend/static/server/frontend/src/components/Register/Register.jsx
@@ -1,1 +1,19 @@
+import React, { useState } from 'react';
 
+const Register = () => {
+    return (
+        <div>
+            <h2>Sign Up</h2>
+            <form>
+                <input type="text" placeholder="Username" required />
+                <input type="text" placeholder="First Name" required />
+                <input type="text" placeholder="Last Name" required />
+                <input type="email" placeholder="Email" required />
+                <input type="password" placeholder="Password" required />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
